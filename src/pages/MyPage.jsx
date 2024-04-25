5ff7768dfd8d228d7962ba063cb56729a6029b25
Sujeong Kim <lewis14081626@gmail.com>
2024-04-26 01:32:36 +0900

    fix: ProfileModify, PasswordModify 컴포넌트 추가

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,5 +1,39 @@
+import styled from 'styled-components';
+import MEDIA_QUERIES from '../constants/MEDIA_QUERIES';
+import BackBtn from '../components/MyPage/BackBtn';
+import ProfileModify from '../components/MyPage/ProfileModify';
+import PasswordModify from '../components/MyPage/PasswordModify';
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <Div>
+      <ContentDiv>
+        <ContentBox>
+          <BackBtn />
+          <ProfileModify name="닉네임을 입력해주세요" />
+          <PasswordModify />
+        </ContentBox>
+      </ContentDiv>
+    </Div>
+  );
 };
 
 export default MyPage;
+
+const Div = styled.div`
+  display: flex;
+`;
+
+const ContentDiv = styled.div`
+  flex: 1;
+`;
+
+const ContentBox = styled.div`
+  height: 101rem;
+  padding: 2rem 0 0 2rem;
+  background-color: ${({ theme }) => theme.color.background};
+
+  ${MEDIA_QUERIES.onTablet} {
+    height: 111rem;
+  }
+`;
