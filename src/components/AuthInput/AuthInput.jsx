f1bbf4f1a7988a821f6bd6d6c8501420b33daac9
jinah-dev <kimjinah.dev@gmail.com>
2024-04-27 15:35:30 +0900

    Refactor : AuthInput 리팩토링

--- src/components/AuthInput/AuthInput.jsx
+++ src/components/AuthInput/AuthInput.jsx
@@ -1,55 +1,58 @@
-import { useState } from 'react';
 import * as S from './styled';
+import { forwardRef } from 'react';
+import useBooleanState from '../../hooks/useBooleanState';
 
-function AuthInput({ data, placeholder, title, wrong, handleBlur, handleChange, value, test }) {
-  const [pwd, setPwd] = useState(true);
-
-  const handlePwd = () => {
-    setPwd((prev) => !prev);
-  };
+const AuthInput = forwardRef(function AuthInput(
+  { name, data, placeholder, label, error, onBlur, onChange, value },
+  ref,
+) {
+  // eslint-disable-next-line no-unused-vars
+  const [isPassword, _, __, togglePassword] = useBooleanState();
 
   return (
     <>
       {data !== 'pwd' ? (
         <S.inputWrap>
-          <S.label htmlFor={data}>{title}</S.label>
+          <S.label htmlFor={data}>{label}</S.label>
           <S.input
-            onBlur={handleBlur}
+            name={name}
+            ref={ref}
             type={data === '이메일' ? 'email' : 'text'}
             id={data}
             placeholder={placeholder}
-            onChange={handleChange}
+            onChange={onChange}
             value={value}
-            onFocus={test}
-            wrong={wrong}
-          ></S.input>
-          {wrong && data === '이메일' && <S.wrong>{data} 형식으로 작성해 주세요.</S.wrong>}
-          {wrong && data === '닉네임' && <S.wrong>10자 이하로 작성해주세요.</S.wrong>}
+            onBlur={onBlur}
+            error={error}
+          />
+          {error && data === '이메일' && <S.wrong>{data} 형식으로 작성해 주세요.</S.wrong>}
+          {error && data === '닉네임' && <S.wrong>10자 이하로 작성해주세요.</S.wrong>}
         </S.inputWrap>
       ) : (
         <S.inputWrap>
-          <S.label htmlFor={data + title}>{title}</S.label>
+          <S.label htmlFor={data + label}>{label}</S.label>
           <S.inputInner>
             <S.input
-              type={pwd ? 'password' : 'text'}
-              id={data + title}
+              name={name}
+              ref={ref}
+              type={isPassword ? 'password' : 'text'}
+              id={data + label}
               placeholder={placeholder}
-              onChange={handleChange}
-              onBlur={handleBlur}
+              onChange={onChange}
+              onBlur={onBlur}
               value={value}
-              onFocus={test}
-              wrong={wrong}
-            ></S.input>
-            <S.imageWrap onClick={handlePwd}>
-              {pwd ? (
+              error={error}
+            />
+            <S.imageWrap onClick={togglePassword}>
+              {isPassword ? (
                 <img src="src/assets/icon/eyeOffIcon.svg" alt="비밀번호 숨기려면 클릭" />
               ) : (
                 <img src="src/assets/icon/eyeOnIcon.svg" alt="비밀번호 숨기려면 클릭" />
               )}
             </S.imageWrap>
           </S.inputInner>
-          {wrong &&
-            (title === '비밀번호' ? (
+          {error &&
+            (label === '비밀번호' ? (
               <S.wrong>8자 이상 입력해 주세요.</S.wrong>
             ) : (
               <S.wrong>비밀번호를 확인해 주세요.</S.wrong>
@@ -58,6 +61,6 @@ function AuthInput({ data, placeholder, title, wrong, handleBlur, handleChange, 
       )}
     </>
   );
-}
+});
 
 export default AuthInput;
