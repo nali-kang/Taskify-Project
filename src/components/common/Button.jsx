05264e899d72a451545b119a03f67df276195b51
Sujeong Kim <lewis14081626@gmail.com>
2024-04-28 12:06:27 +0900

    fix: props가 Button에서 전달되게 수정

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,34 +1,34 @@
+/* eslint-disable no-console */
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { BUTTON_TYPE } from '../../constants/BUTTON_TYPE';
 import { buttonStyleBySize, buttonStyleByType } from '../../styles/ButtonStyles';
-const Btn = styled.button`
-  padding: 0.5rem 1rem;
-  white-space: nowrap;
-`;
 
-Btn.defaultProps = ({ size, $styleType }) => ({
-  ...buttonStyleBySize[size],
-  ...buttonStyleByType[$styleType],
-  '&:disabled': {
-    backgroundColor: 'gray',
-    cursor: 'not-allowed',
-  },
-});
-
-function Button({ children, styleType = BUTTON_TYPE.PRIMARY, size = 'M', ...htmlButtonProps }) {
+const Button = ({ children, styleType = BUTTON_TYPE.PRIMARY, size = 'M', ...htmlButtonProps }) => {
   return (
     <Btn {...htmlButtonProps} size={size} $styleType={styleType}>
       {children}
     </Btn>
   );
-}
+};
 
-// props 검증 -> propTypes: 입력 받은 타입을 검증
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   size: PropTypes.oneOf(['S', 'M', 'L']),
   styleType: PropTypes.oneOf(Object.keys(BUTTON_TYPE)),
 };
+
+const Btn = styled.button`
+  padding: 0.5rem 1rem;
+  white-space: nowrap;
+  ${({ size, $styleType }) => `
+    ${buttonStyleBySize[size]}
+    ${buttonStyleByType[$styleType]}
+  `}
+  &:disabled {
+    background-color: gray;
+    cursor: 'not-allowed';
+  }
+`;
 
 export default Button;
