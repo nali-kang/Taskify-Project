25d9afa717a81a3225c91e97a365ed7bd6d0853d
jinah-dev <kimjinah.dev@gmail.com>
2024-04-30 00:52:16 +0900

    Refactor : ToDoModal 리팩토링

--- src/components/Modal/ToDoModal/ShowToDo/ToDoModal.jsx
+++ src/components/Modal/ToDoModal/ShowToDo/ToDoModal.jsx
@@ -6,10 +6,14 @@ import ToDoModalOption from './ToDoModalOption';
 import ToDoModalTag from './ToDoModalTag';
 import ToDoModalUser from './ToDoModalUser';
 import BaseModal from '../../../common/Modal';
+import useBooleanState from '../../../../hooks/useBooleanState';
+import { useMutation } from '@tanstack/react-query';
+import instance from '../../../../lib/axios';
 
 const ToDoModal = ({
+  id,
   isOpen,
-  colseModal,
+  closeModal,
   columnName,
   imageUrl,
   assignee,
@@ -17,11 +21,25 @@ const ToDoModal = ({
   description,
   dueDate,
   tags,
+  setCardList,
   // img,
 }) => {
+  // FIXME: startEdit 수정
+  const [isEdit, startEdit] = useBooleanState();
+
+  // eslint-disable-next-line no-unused-vars
+  const [isOpenTodoModalOption, _, closeTodoModalOption, toggleTodoModalOption] = useBooleanState();
+
+  const { mutateAsync: deleteCard } = useMutation({
+    mutationFn: () => {
+      const { data } = instance.delete(`/cards/${id}`);
+
+      return data;
+    },
+  });
+
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
-  const [renderedOption, setRenderedOption] = useState(null);
   // const [error, setError] = useState(null); // 여기에 error 상태 추가
 
   const handleCommentChange = (e) => {
@@ -48,18 +66,6 @@ const ToDoModal = ({
   //   setComment("");
   // };
 
-  const onClickModalOption = () => {
-    if (renderedOption) {
-      setRenderedOption(null);
-    } else {
-      setRenderedOption(<ToDoModalOption />);
-    }
-  };
-
-  const onClickModalClose = () => {
-    colseModal();
-  };
-
   const handleEditComment = (id, editedComment) => {
     setComments((prevComments) => {
       return prevComments.map((comment, i) =>
@@ -74,69 +80,86 @@ const ToDoModal = ({
     setComments(updatedComments);
   };
 
+  const handleClickDelete = async () => {
+    await deleteCard();
+
+    setCardList((prev) => prev.filter((card) => card.id !== id));
+
+    closeTodoModalOption();
+    closeModal();
+  };
+
   return (
     // 커밋오류때문에 추가한 부분
     <>
       {/* {error && <p style={{ color: 'red' }}>{error}</p>} */}
 
-      <BaseModal isOpen={isOpen}>
-        <S.ModalContainer>
-          <S.ModalHeader>
-            <h1>{title}</h1>
-            <div className="button_area">
-              <button className="modal_button" onClick={onClickModalOption}>
-                <img src="/src/assets/icon/3dot.svg" />
-              </button>
-              <button className="modal_button" onClick={onClickModalClose}>
-                <img src="/src/assets/icon/closeX.svg" />
-              </button>
-              {renderedOption}
-            </div>
-          </S.ModalHeader>
-          <ToDoModalUser user={assignee} deadline={dueDate} />
-          <S.ModalContent>
-            <S.ModalTag>
-              <div>
-                <h1></h1>
-                <h2>{columnName}</h2>
+      {isEdit ? (
+        <BaseModal isOpen>
+          <div>수정하는 모달</div>
+        </BaseModal>
+      ) : (
+        <BaseModal isOpen={isOpen}>
+          <S.ModalContainer>
+            <S.ModalHeader>
+              <h1>{title}</h1>
+              <div className="button_area">
+                <button className="modal_button" onClick={toggleTodoModalOption}>
+                  <img src="/src/assets/icon/3dot.svg" />
+                </button>
+                <button className="modal_button" onClick={closeModal}>
+                  <img src="/src/assets/icon/closeX.svg" />
+                </button>
+                {isOpenTodoModalOption && (
+                  <ToDoModalOption onClickModify={startEdit} onClickDelete={handleClickDelete} />
+                )}
               </div>
-              <p>|</p>
-              <ToDoModalTag tags={tags} />
-            </S.ModalTag>
-            <S.ModalWords>{description}</S.ModalWords>
-            {imageUrl && (
-              <S.ModalContentImage>
-                <img src={imageUrl} alt="img" />
-              </S.ModalContentImage>
-            )}
+            </S.ModalHeader>
+            <ToDoModalUser user={assignee} deadline={dueDate} />
+            <S.ModalContent>
+              <S.ModalTag>
+                <div>
+                  <h1></h1>
+                  <h2>{columnName}</h2>
+                </div>
+                <p>|</p>
+                <ToDoModalTag tags={tags} />
+              </S.ModalTag>
+              <S.ModalWords>{description}</S.ModalWords>
+              {imageUrl && (
+                <S.ModalContentImage>
+                  <img src={imageUrl} alt="img" />
+                </S.ModalContentImage>
+              )}
 
-            <S.ModalCommentInput>
-              <h3>댓글</h3>
-              <div>
-                <textarea
-                  placeholder="댓글 작성하기"
-                  value={comment}
-                  onChange={handleCommentChange}
-                />
-                <Button>입력</Button>
-                {/* <Button onClick={handleCommentSubmit}>입력</Button> */}
-              </div>
-              <ul>
-                {comments.map((commentItem, index) => (
-                  <ToDoModalComment
-                    key={index}
-                    id={index}
-                    user={assignee}
-                    comment={commentItem}
-                    onEditComment={handleEditComment}
-                    onDeleteComment={() => handleDeleteComment(index)}
+              <S.ModalCommentInput>
+                <h3>댓글</h3>
+                <div>
+                  <textarea
+                    placeholder="댓글 작성하기"
+                    value={comment}
+                    onChange={handleCommentChange}
                   />
-                ))}
-              </ul>
-            </S.ModalCommentInput>
-          </S.ModalContent>
-        </S.ModalContainer>
-      </BaseModal>
+                  <Button>입력</Button>
+                  {/* <Button onClick={handleCommentSubmit}>입력</Button> */}
+                </div>
+                <ul>
+                  {comments.map((commentItem, index) => (
+                    <ToDoModalComment
+                      key={index}
+                      id={index}
+                      user={assignee}
+                      comment={commentItem}
+                      onEditComment={handleEditComment}
+                      onDeleteComment={() => handleDeleteComment(index)}
+                    />
+                  ))}
+                </ul>
+              </S.ModalCommentInput>
+            </S.ModalContent>
+          </S.ModalContainer>
+        </BaseModal>
+      )}
     </>
   );
 };
